9e5e01164a913cdc693a49673f21a2033b97a03f
FaiqaMuhammadYousuf <faiqamuhammadyousuf@gmail.com>
2024-02-14 06:38:43 -0800

    Odd Numbers

---
+++ Odd/Odd/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Odd Numbers");
+            for (int i = 1; i <= 10; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
